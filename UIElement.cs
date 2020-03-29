536d291643b6401925899ae4d8afb74d5f44af53
Graumen <62729624+Graumen@users.noreply.github.com>
2020-03-29 05:37:55 +0200

    Add files via upload

--- UIElement.cs
+++ UIElement.cs
@@ -12,7 +12,7 @@ namespace Widgets
             var pp = (ci.ftf ? 1 : 0) * (lp.Top - Main.screenPosition + new Vector2(0, lp.gfxOffY));
 
             sb.Draw(GetTexture("Widgets/Background"), new Vector2((int)pp.X + ci.ftx, (int)pp.Y + ci.fty), Color.White);
-            sb.Draw(GetTexture("Widgets/FlightTimeBar"), new Rectangle((int)pp.X + 28 + ci.ftx, (int)pp.Y + 14 + ci.fty, (int)lp.wingTime * 75 / ((0 < lp.rocketBoots ? 42 : 0) + lp.wingTimeMax), 4), Color.White);
+            sb.Draw(GetTexture("Widgets/FlightTimeBar"), new Rectangle((int)pp.X + 28 + ci.ftx, (int)pp.Y + 14 + ci.fty, (int)lp.wingTime * 75 / ((0 == lp.rocketTime ? 42 : 0) + lp.wingTimeMax), 4), Color.White);
         }
     }
 }
