626f86410b40c8c7cce92bcfeeeb175bdfe89139
Graumen <62729624+Graumen@users.noreply.github.com>
2020-03-29 03:39:40 +0200

    Add files via upload

--- Mod.cs
+++ Mod.cs
@@ -1,60 +1,47 @@
 using Microsoft.Xna.Framework;
+using static Terraria.ModLoader.ModContent;
 using System;
 using Terraria.ID;
-using Terraria.ModLoader;
 using Terraria.UI;
 using Terraria;
 namespace Widgets
 {
-    class Mod0 : Mod
+    class Mod0 : Terraria.ModLoader.Mod
     {
         Item ca = new Item();
-        static Config ci = ModContent.GetInstance<Config>();
-        static Player lp = Main.LocalPlayer;
-        UserInterface ft = new UserInterface();
-        Color Rarity(int a)
+        UserInterface ui = new UserInterface();
+        Color Rarity()
         {
-            switch (a)
+            switch (ca.rare)
             {
-                case -11:
-                    return new Color(255, 175, 0);
-                case -1:
-                    return Colors.RarityTrash;
-                case 0:
-                    return Colors.RarityNormal;
-                case 1:
-                    return Colors.RarityBlue;
-                case 2:
-                    return Colors.RarityGreen;
-                case 3:
-                    return Colors.RarityOrange;
-                case 4:
-                    return Colors.RarityRed;
-                case 5:
-                    return Colors.RarityPink;
-                case 6:
-                    return Colors.RarityPurple;
-                case 7:
-                    return Colors.RarityLime;
-                case 8:
-                    return Colors.RarityYellow;
-                case 9:
-                    return Colors.RarityCyan;
-                case 10:
-                    return new Color(255, 40, 100);
+                case -11: return new Color(255, 175, 0);
+                case -1: return Colors.RarityTrash;
+                case 0: return Colors.RarityNormal;
+                case 1: return Colors.RarityBlue;
+                case 2: return Colors.RarityGreen;
+                case 3: return Colors.RarityOrange;
+                case 4: return Colors.RarityRed;
+                case 5: return Colors.RarityPink;
+                case 6: return Colors.RarityPurple;
+                case 7: return Colors.RarityLime;
+                case 8: return Colors.RarityYellow;
+                case 9: return Colors.RarityCyan;
+                case 10: return new Color(255, 40, 100);
             }
             return new Color(180, 40, 255);
         }
         public override void Load()
         {
-            ft.SetState(new UIState());
             new UIState().Activate();
+            ui.SetState(new UIState());
         }
-        public override void ModifyInterfaceLayers(System.Collections.Generic.List<GameInterfaceLayer> layers)
+        public override void ModifyInterfaceLayers(System.Collections.Generic.List<GameInterfaceLayer> l)
         {
+            var ci = GetInstance<Config>();
             var it = Main.itemTexture[ca.type];
+            var lp = Main.LocalPlayer;
             var min = 1 > Math.Min((float)ci.cah / it.Height, (float)ci.caw / it.Width) / 1.375f ? Math.Min((float)ci.cah / it.Height, (float)ci.caw / it.Width) / 1.375f : 1;
-            var mti = layers.FindIndex(a => a.Name == "Vanilla: Mouse Text");
+            var mti = l.FindIndex(a => a.Name == "Vanilla: Mouse Text");
             var pp = (ci.caf ? 1 : 0) * (lp.Top - Main.screenPosition + new Vector2(0, lp.gfxOffY));
             var sb = Main.spriteBatch;
 
@@ -79,14 +66,15 @@ namespace Widgets
 
             if (mti != -1)
             {
-                layers.Insert(mti, new LegacyGameInterfaceLayer("", () =>
+                l.Insert(mti, new LegacyGameInterfaceLayer("", () =>
                {
                    if (-1 == lp.grappling[0]
                     && !lp.dead
+                    && !lp.mount.Active
                     && 0 != lp.velocity.Y
                     && 0 < lp.wingTime
                     && 0 == lp.jump
-                    && ci.ftv) { ft.Draw(sb, new GameTime()); }
+                    && ci.ftv) { ui.Draw(sb, new GameTime()); }
                    if (!lp.dead
                     && 0 < lp.HeldItem.useAmmo
                     && ci.cav
@@ -94,26 +82,17 @@ namespace Widgets
                    {
                        if (0 < ci.cac.A) { Utils.DrawInvBG(sb, new Rectangle((int)pp.X + ci.cax, (int)pp.Y + ci.cay, ci.caw, ci.cah), ci.cac); }
                        sb.Draw(it, new Rectangle((int)Math.Round((ci.caw - it.Width * min) / 2 + ci.cax) + (int)pp.X, (int)Math.Round((ci.cah - it.Height * min) / 2 + ci.cay) + (int)pp.Y, (int)Math.Round(it.Width * min), (int)Math.Round(it.Height * min)), Color.White);
-                       Utils.DrawBorderString(sb, ca.Name + (1 < ca.stack ? "\n" + ca.stack : ""), new Vector2((int)pp.X + 10 + ci.caw + ci.cax, (int)pp.Y + ci.cay), Rarity(ca.rare));
+                       Utils.DrawBorderString(sb, ca.Name + (1 < ca.stack ? "\n" + ca.stack : ""), new Vector2((int)pp.X + 10 + ci.caw + ci.cax, (int)pp.Y + ci.cay), Rarity());
                    }
                    if (!lp.dead
                     && ci.muv)
                    {
                        sb.Draw(Main.buffTexture[150], new Vector2(ci.mux, ci.muy), Color.White);
-                       Utils.DrawBorderString(sb, $"{lp.slotsMinions}/{lp.maxMinions}", new Vector2((Main.buffTexture[150].Width - Main.fontMouseText.MeasureString($"{lp.slotsMinions}/{lp.maxMinions}").X) / 2 + ci.mux, 35 + ci.muy), Color.White);
+                       Utils.DrawBorderString(sb, $"{lp.slotsMinions}/{lp.maxMinions}", new Vector2((Main.buffTexture[150].Width - Main.fontMouseText.MeasureString($"{lp.slotsMinions}/{lp.maxMinions}").X) / 2 + ci.mux, 3 + ci.muy + Main.buffTexture[150].Height), Color.White);
                    }
                    return true;
                }, InterfaceScaleType.UI));
             }
-        }
-        public override void UpdateUI(GameTime gameTime)
-        {
-            if (-1 == lp.grappling[0]
-             && !lp.dead
-             && 0 != lp.velocity.Y
-             && 0 < lp.wingTime
-             && 0 == lp.jump
-             && ci.ftv) { ft?.Update(gameTime); }
         }
     }
 }
--- UIElement.cs
+++ UIElement.cs
@@ -5,14 +5,14 @@ namespace Widgets
 {
     class FlightTimeBar : Terraria.UI.UIElement
     {
-        protected override void DrawSelf(Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch)
+        protected override void DrawSelf(Microsoft.Xna.Framework.Graphics.SpriteBatch sb)
         {
             var ci = GetInstance<Config>();
             var lp = Main.LocalPlayer;
             var pp = (ci.ftf ? 1 : 0) * (lp.Top - Main.screenPosition + new Vector2(0, lp.gfxOffY));
 
-            spriteBatch.Draw(GetTexture("Widgets/Background"), new Vector2((int)pp.X + ci.ftx, (int)pp.Y + ci.fty), Color.White);
-            spriteBatch.Draw(GetTexture("Widgets/FlightTimeBar"), new Rectangle((int)pp.X + 28 + ci.ftx, (int)pp.Y + 14 + ci.fty, (int)lp.wingTime * 75 / ((0 < lp.rocketBoots ? 42 : 0) + lp.wingTimeMax), 4), Color.White);
+            sb.Draw(GetTexture("Widgets/Background"), new Vector2((int)pp.X + ci.ftx, (int)pp.Y + ci.fty), Color.White);
+            sb.Draw(GetTexture("Widgets/FlightTimeBar"), new Rectangle((int)pp.X + 28 + ci.ftx, (int)pp.Y + 14 + ci.fty, (int)lp.wingTime * 75 / ((0 < lp.rocketBoots ? 42 : 0) + lp.wingTimeMax), 4), Color.White);
         }
     }
 }
